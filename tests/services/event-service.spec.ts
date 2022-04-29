5dc2369c6ff098172570422e1fd6a357eb0dbb52
Yan Soares <yts.1995@gmail.com>
2022-04-29 12:34:28 -0300

    tes: finish event service post deposit

--- tests/services/event-service.spec.ts
+++ tests/services/event-service.spec.ts
@@ -1,23 +1,83 @@
+import { RequestError } from '@/errors'
 import { EventRepository } from '@/repositories'
 import { EventService } from '@/services'
-import { mockAccountWithInitialBalance } from '@/tests/mocks'
+import { mockDeposit, mockTransfer, mockWithdraw } from '@/tests/mocks'
 
 describe('EventService', () => {
-  const eventRepository = {} as EventRepository
-  const eventService = new EventService()
-  it('should be able to create a new account with initial balance', () => {
-    const response = eventService.create(mockAccountWithInitialBalance)
+  let eventRepository: EventRepository
+  let eventService: EventService
 
-    expect(response.destination).toHaveProperty('id')
+  beforeEach(() => {
+    eventRepository = {} as EventRepository
+    eventService = new EventService()
   })
 
-  it('should be able to deposit into existing account', () => {
-    eventRepository.findByDestination = jest.fn().mockReturnValue(true)
-    eventService.create(mockAccountWithInitialBalance)
+  describe('DEPOSIT', () => {
+    it('should be able to create a new account with initial balance', () => {
+      eventRepository.findByAccountNumber = jest.fn().mockReturnValue(undefined)
+      const depositResponse = eventService.execute(mockDeposit)
 
-    const response = eventService.create(mockAccountWithInitialBalance)
+      expect(depositResponse.destination).toHaveProperty('id')
+      expect(depositResponse.destination.balance).toEqual(10)
+    })
 
-    expect(response.destination).toHaveProperty('id')
-    expect(response.destination.balance).toEqual(20)
+    it('should be able to deposit into existing account', () => {
+      eventRepository.findByAccountNumber = jest.fn().mockReturnValue(true)
+      eventService.execute(mockDeposit)
+
+      const depositResponse = eventService.execute(mockDeposit)
+
+      expect(depositResponse.destination).toHaveProperty('id')
+      expect(depositResponse.destination.balance).toEqual(20)
+    })
+  })
+
+  describe('WITHDRAW', () => {
+    it('should be able to withdraw from a existing account', () => {
+      eventRepository.findByAccountNumber = jest.fn().mockReturnValue(true)
+      eventService.execute(mockDeposit)
+
+      const withdrawResponse = eventService.execute(mockWithdraw)
+
+      expect(withdrawResponse.destination).toHaveProperty('id')
+      expect(withdrawResponse.destination.balance).toEqual(0)
+    })
+
+    it('should not be able to withdraw from a non-existing account', () => {
+      eventRepository.findByAccountNumber = jest.fn().mockRejectedValueOnce(undefined)
+      const error = new RequestError('Account does not exists.')
+
+      expect(() => eventService.execute(mockWithdraw)).toThrow(error.message)
+    })
+  })
+
+  describe('TRANSFER', () => {
+    it('should be able to transfer from a existing account', () => {
+      eventRepository.findByAccountNumber = jest.fn().mockReturnValue(true)
+      eventService.execute({
+        ...mockDeposit,
+        amount: 15
+      })
+
+      const transferResponse = eventService.execute({
+        ...mockTransfer,
+        amount: 0,
+        account: '100'
+      })
+
+      expect(transferResponse.destination).toHaveProperty('id')
+      expect(transferResponse.destination.balance).toEqual(15)
+    })
+
+    it('should not be able to transfer from a non-existing account', () => {
+      eventRepository.findByAccountNumber = jest.fn().mockRejectedValueOnce(undefined)
+      const error = new RequestError('Account does not exists.')
+
+      expect(() => eventService.execute({
+        ...mockTransfer,
+        amount: 0,
+        account: '100'
+      })).toThrow(error.message)
+    })
   })
 })
